36c531d47b1e58d46eccfb73733c58f517465a9a
Braxton Jackson <Braxton.Jackson7@protonmail.com>
2024-12-04 15:23:06 -0500

    added routing for the budget and income tax calculators

---
+++ client/src/routes/AppRouter.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import routes from '.';
+
+const AppRouter = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				{routes.map((route, index) => (
+					<Route
+						key={index}
+						path={route.path}
+						element={route.element}
+					/>
+				))}
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default AppRouter;
