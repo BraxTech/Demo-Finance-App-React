7f308bd1c69a3398aa9eab9d1722df85bb5f21ad
Braxton Jackson <Braxton.Jackson7@protonmail.com>
2024-12-28 14:25:00 -0500

    Responsiveness changes for income tax calculator page

--- client/src/components/feature/IncomeTaxCalculator/index.tsx
+++ client/src/components/feature/IncomeTaxCalculator/index.tsx
@@ -1,25 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const states = [
+	'Alabama',
+	'Alaska',
+	'Arizona',
+	'Arkansas',
+	'California',
+	'Colorado',
+	'Connecticut',
+	'Delaware',
+	'Florida',
+	'Georgia',
+	'Hawaii',
+	'Idaho',
+	'Illinois',
+	'Indiana',
+	'Iowa',
+	'Kansas',
+	'Kentucky',
+	'Louisiana',
+	'Maine',
+	'Maryland',
+	'Massachusetts',
+	'Michigan',
+	'Minnesota',
+	'Mississippi',
+	'Missouri',
+	'Montana',
+	'Nebraska',
+	'Nevada',
+	'New Hampshire',
+	'New Jersey',
+	'New Mexico',
+	'New York',
+	'North Carolina',
+	'North Dakota',
+	'Ohio',
+	'Oklahoma',
+	'Oregon',
+	'Pennsylvania',
+	'Rhode Island',
+	'South Carolina',
+	'South Dakota',
+	'Tennessee',
+	'Texas',
+	'Utah',
+	'Vermont',
+	'Virginia',
+	'Washington',
+	'West Virginia',
+	'Wisconsin',
+	'Wyoming',
+];
 
 const IncomeTaxCalculator = () => {
+	const [grossIncome, setGrossIncome] = useState('');
+	const [state, setState] = useState('');
+
 	return (
-		<div className='bg-[#1D263B] h-[550px] w-[425px] mt-2 rounded-3xl align-center justify-strt text-left flex flex-col p-5 gap-[25px]'>
+		<div className='bg-[#1D263B]  md:w-[641px] mt-2 rounded-3xl align-center justify-start text-left flex flex-col p-5 gap-[25px] mt-[25px] w-full md max-w-full lg:mt-0'>
 			<h1 className='text-[35px] text-center text-white mt-[10px]'>
 				Income Tax Calculator
 			</h1>
-			<div className='mx-[auto] text-center'>
+			<div className='mx-[auto] text-left'>
 				<p className='text-[19px] text-white'>Enter Your Gross Income:</p>
 				<input
 					type='text'
-					className='w-[350px] h-[55px] rounded-lg text-[20px] font-semibold '
+					className='sm:w-[300px] h-[55px] rounded-lg text-[20px] font-semibold p-2 '
+					value={`$${grossIncome}`}
+					onChange={(e) => {
+						setGrossIncome(e.target.value.replace('$', ''));
+						console.log(grossIncome);
+					}}
 				/>
 			</div>
-			<div className='mx-[auto] text-center'>
-				<p className='text-[19px] text-white'>Enter Your Gross Income:</p>
-				<select className='w-[350px] h-[55px] rounded-lg text-[20px] font-semibold ' />
+			<div className='mx-[auto] text-left'>
+				<p className='text-[19px] text-white'>Per:</p>
+				<select className='sm:w-[300px] w-[250px] h-[55px] rounded-lg text-[20px] font-semibold ' />
 			</div>
-			<div className='mx-auto text-center'>
-				<p className='text-[19px] text-white'>Enter Your Gross Income:</p>
-				<select className='w-[350px] h-[55px] rounded-lg text-[20px] font-semibold ' />
+			<div className='mx-auto text-left'>
+				<p className='text-[19px] text-white'>Where do you work?</p>
+				<select
+					className='w-[250px] sm:w-[300px]  h-[55px] rounded-lg text-[20px] font-semibold p-3'
+					onChange={(e) => setState(e.target.value)}
+					value={state}>
+					<option value=''>Select a State</option>
+					{states.map((state) => (
+						<option
+							key={state}
+							value={state}>
+							{state}
+						</option>
+					))}
+				</select>
 			</div>
 
 			<div>
--- client/src/components/feature/RateButton/index.tsx
+++ client/src/components/feature/RateButton/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
 interface RateButtonProps {
-  label: string;
+	label: string;
+	isSelected: boolean;
+	onClick: () => void;
 }
 
-const RateButton = ({ label }: RateButtonProps) => {
-  return (
-    <div className=' w-[145px]  h-[54px] my-auto rounded-lg flex justify-center items-center text-gray-400 text-[18px]'>
-      <p>{label}</p>
-    </div>
-  );
-}
+const RateButton = ({ label, isSelected, onClick }: RateButtonProps) => {
+	return (
+		<div
+			className={`flex-shrink-0 w-[105px] md:w-[100px] h-[60px] my-auto rounded-lg flex justify-center items-center text-[16px] md:text-[18px] cursor-pointer 
+				${isSelected ? 'bg-red-500 text-white' : 'text-gray-400 hover:bg-gray-200'}`}
+			onClick={onClick}>
+			<p>{label}</p>
+		</div>
+	);
+};
 
-export default RateButton; 
+export default RateButton;
