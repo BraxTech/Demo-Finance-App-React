1fcea9fda59ad6a6ba4498d985ce3b723d80cd45
Braxton Jackson <Braxton.Jackson7@protonmail.com>
2024-12-28 14:58:43 -0500

    vite config changeS

---
+++ client/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@app': path.resolve(__dirname, 'src/app'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+        },
+    },
+});
